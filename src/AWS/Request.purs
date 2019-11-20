d3e9c4e5d9b097df5956ef9c8a8559041019e5f7
Jan Vincent Liwanag <jvliwanag@gmail.com>
2019-11-20 13:04:02 +0800

    Add unsafeRequest

--- src/AWS/Request.purs
+++ src/AWS/Request.purs
@@ -1,6 +1,7 @@
 module AWS.Request
        ( MethodName
        , request
+       , unsafeRequest
        ) where
 
 import Prelude
@@ -9,6 +10,7 @@ import AWS.Service (Service)
 import Effect.Aff (Aff)
 import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 import Foreign (Foreign)
+import Unsafe.Coerce (unsafeCoerce)
 
 type MethodName = String
 
@@ -17,3 +19,8 @@ foreign import requestImpl :: Service -> MethodName -> Foreign -> EffectFnAff Fo
 request :: Service -> MethodName -> Foreign -> Aff Foreign
 request service methodName i =
   requestImpl service methodName i # fromEffectFnAff
+
+--| Only call `unsafeRequest` for auto generated code that guarantees
+--| that the `input` and `output` types are correct.
+unsafeRequest :: forall input output. Service -> MethodName -> input -> Aff output
+unsafeRequest = unsafeCoerce request
